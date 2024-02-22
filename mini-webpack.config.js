4d13fca8f3def08b12d0fddc77a0abd001da4e89
linhuizi <529246402@qq.com>
2024-02-22 17:43:44 +0800

    fix: update mini webpack config

---
+++ mini-webpack.config.js
@@ -0,0 +1,25 @@
+// mini-webpack-config 配置文件
+// 导出一个对象
+// webpack 的配置文件是 JavaScript 文件，文件内导出了一个 webpack 配置的对象。 
+// webpack 会根据该配置定义的属性进行处理。
+// webpack 遵循 CommonJS 模块规范，所以配置文件可以使用 require
+// export default {
+//   // 定义入口文件
+//   entry: './src/index.js',
+//   // 定义输出文件
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   }
+// }
+const path = require('path');
+
+module.exports = {
+  // 定义入口文件
+  entry: './example/hi.js',
+  // 定义输出文件
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  }
+}
