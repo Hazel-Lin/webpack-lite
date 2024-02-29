af19dc28e47a2437d2bc8a9881828a441b45ead8
linhuizi <529246402@qq.com>
2024-02-29 15:18:40 +0800

    fix: update config

--- mini-webpack.config.js
+++ mini-webpack.config.js
@@ -3,18 +3,9 @@
 // webpack 的配置文件是 JavaScript 文件，文件内导出了一个 webpack 配置的对象。 
 // webpack 会根据该配置定义的属性进行处理。
 // webpack 遵循 CommonJS 模块规范，所以配置文件可以使用 require
-// export default {
-//   // 定义入口文件
-//   entry: './src/index.js',
-//   // 定义输出文件
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist')
-//   }
-// }
+
 const path = require('path');
-const jsonLoader = require('./example/jsonLoader');
-const myLoader = require('./example/myLoader.js');
+
 module.exports = {
   // 定义入口文件
   entry: './example/hi.js',
@@ -25,8 +16,18 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.json$/, loader: jsonLoader },
-      { test: /\.js$/, loader:myLoader},
+      // 先执行 changeLoader  再执行 myLoader 在这个配置中 use 中的元素应该是字符串的形式
+      {
+        test: /\.js$/,
+        use: [
+          path.resolve(__dirname, 'example/changeLoader.js'),
+          path.resolve(__dirname, 'example/myLoader.js')
+        ]
+      },
+      {
+        test: /\.json$/,
+        use: [path.resolve(__dirname, 'example/jsonLoader.js')]
+      },
     ]
   }
 }
